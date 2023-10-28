5676edea855586261fb771d3fc69aba0560811f2
Adéla Kohoutková <130575660+Craterisia@users.noreply.github.com>
2023-10-28 21:09:18 +0000

    Finishied functions

--- toDoList/script.js
+++ toDoList/script.js
@@ -8,6 +8,12 @@ const listDisplayContainer = document.querySelector(
 const listTitleElement = document.querySelector("[data-list-title]");
 const listCountElement = document.querySelector("[data-list-count]");
 const tasksContainer = document.querySelector("[data-tasks]");
+const taskTemplate = document.getElementById("task-template");
+const newTaskForm = document.querySelector("[data-new-task-form]");
+const newTaskInput = document.querySelector("[data-new-task-input]");
+const clearCompleteTasksButton = document.querySelector(
+  "[data-clear-complete-tasks-button]"
+);
 
 // Nastavení výchozího prázdného pole nebo vytažení seznamu uloženého v local storage a Zachycení aktivace seznamu úkolů
 const LOCAL_STORAGE_LIST_KEY = "task.lists";
@@ -21,6 +27,26 @@ listsContainer.addEventListener("click", (e) => {
     selectedListId = e.target.dataset.listId;
     saveAndRender();
   }
+});
+
+// Zachycení splnění podúkolu
+tasksContainer.addEventListener("click", (e) => {
+  if (e.target.tagName.toLowerCase() === "input") {
+    const selectedList = lists.find((list) => list.id === selectedListId);
+    const selectedTask = selectedList.tasks.find(
+      (task) => task.id === e.target.id
+    );
+    selectedTask.complete = e.target.checked;
+    save();
+    renderTaskCount(selectedList);
+  }
+});
+
+// Zachycení kliknutí na button na vymazání splněných podúkolů
+clearCompleteTasksButton.addEventListener("click", (e) => {
+  const selectedList = lists.find((list) => list.id === selectedListId);
+  selectedList.tasks = selectedList.tasks.filter((task) => !task.complete);
+  saveAndRender();
 });
 
 // Zachycení kliknutí na button na vymazání seznamu úkolů
@@ -44,11 +70,34 @@ newListForm.addEventListener("submit", (e) => {
   saveAndRender();
 });
 
+// Zachycení nového podseznamu úkolů
+newTaskForm.addEventListener("submit", (e) => {
+  // Vypnutí automatického znovuobnovení stránky
+  e.preventDefault();
+
+  // Vytváření nového podseznamu úkolů
+  const taskName = newTaskInput.value;
+  if (taskName == null || taskName == "") return;
+  const task = createTask(taskName);
+  newTaskInput.value = null;
+  const selectedList = lists.find((list) => list.id === selectedListId);
+  selectedList.tasks.push(task);
+  saveAndRender();
+});
+
 function createList(name) {
   return {
     id: Date.now().toString(),
     name: name,
     tasks: [],
+  };
+}
+
+function createTask(name) {
+  return {
+    id: Date.now().toString(),
+    name: name,
+    complete: false,
   };
 }
 
@@ -69,10 +118,42 @@ function render() {
   clearElement(listsContainer);
   renderLists();
 
+  const selectedList = lists.find((list) => list.id === selectedListId);
   if (selectedListId == null) {
     listDisplayContainer.style.display = "none";
   } else {
+    // prázdný string vrátí původní hodnotu
+    listDisplayContainer.style.display = "";
+    // Změna názvu podúkolů
+    listTitleElement.innerText = selectedList.name;
+    // Počitadlo zbývajících úkolů
+    renderTaskCount(selectedList);
+    clearElement(tasksContainer);
+    renderTasks(selectedList);
   }
+}
+
+// Vytvoření podúkolů
+function renderTasks(selectedList) {
+  selectedList.tasks.forEach((task) => {
+    const taskElement = document.importNode(taskTemplate.content, true);
+    const checkbox = taskElement.querySelector("input");
+    checkbox.id = task.id;
+    checkbox.checked = task.complete;
+    const label = taskElement.querySelector("label");
+    label.htmlFor = task.id;
+    label.append(task.name);
+    tasksContainer.appendChild(taskElement);
+  });
+}
+
+// Počitalo zbývajících úkolů
+function renderTaskCount(selectedList) {
+  const incompleteTaskCount = selectedList.tasks.filter(
+    (task) => !task.complete
+  ).lenght;
+  const taskString = incompleteTaskCount === 1 ? "task" : "tasks";
+  listCountElement.innerText = `${incompleteTaskCount} ${taskString} remaining`;
 }
 
 // Vytvoření html podkladu pro nový seznam úkolů (tag li, třída, připnutí, aktivní)
